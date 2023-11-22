a4835e5c1a0461b37e0daa8b1d5b7be979568398
Robin Jacob Plumley <jrplumley@gmail.com>
2023-11-22 13:30:07 -0500

    added

--- app.py
+++ app.py
@@ -12,14 +12,19 @@ toolbar = DebugToolbarExtension(app)
 
 @app.route('/')
 def get_start_game_form():
+    if request.method == 'POST':
+        boggle_game = Boggle()
+        new_board = boggle_game.make_board()
+        session['board'] = new_board
+        return redirect(url_for('show_board'))
     return render_template('start_game.html')
 
 @app.route('/boggle', methods = ['GET', 'POST'])
 def show_board():
-    if request.method == 'POST':
-        print(request.form.get('word'))
-
-    boggle_game = Boggle()
-    new_board = boggle_game.make_board()
-    session['board'] = new_board
+    if request.is_json:
+        data = request.json
+        word = data.get('word')
+        print(word)
+        
     return render_template('jinja.html')
+
